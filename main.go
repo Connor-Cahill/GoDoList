84ab0314c0d8c2c6e9ad50642e6fcbadd0e5d042
Connor Cahill <connorcahill913@gmail.com>
2019-02-18 14:59:20 -0800

    [Added]: main function in main.go now sets up database and tasks bucket when ran.

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/connor-cahill/goDoList/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/connor-cahill/goDoList/cmd"
+	"github.com/connor-cahill/goDoList/db"
+	homedir "github.com/mitchellh/go-homedir"
+)
 
 func main() {
-	cmd.RootCmd.Execute()
+	home, _ := homedir.Dir()                  //	finds users home directory
+	dbPath := filepath.Join(home, "tasks.db") //	creates a tasks.db file in home dir
+	must(db.Init(dbPath))                     //	opens or creates the db and task bucket
+
+	must(cmd.RootCmd.Execute()) //	executes our CLI package
+}
+
+//must is an error handler that simply prints error if there is one
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
